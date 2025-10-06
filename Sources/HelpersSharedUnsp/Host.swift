b88f85d3c3306c2f45e7ee5c294391f0a4507b5c
Malik <timurkaev_malik@icloud.com>
2025-10-07 02:15:09 +0300

    Initial commit

---
+++ Sources/HelpersSharedUnsp/Host.swift
@@ -0,0 +1,13 @@
+//
+//  Host.swift
+//  HelpersSharedUnsp
+//
+//  Created by Malik Timurkaev on 07.10.2025.
+//
+
+import Foundation
+
+public enum Host: String {
+    case unsplash = "unsplash.com"
+    case apiUnsplash = "api.unsplash.com"
+}
